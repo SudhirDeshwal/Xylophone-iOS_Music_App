d2f7b53a1c665155b7ff62c42185c63748724f86
Sudhir Deshwal <DES>
2020-06-18 22:40:59 -0400

    Message got printed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,27 +20,16 @@ class ViewController: UIViewController {
     
     @IBAction func keyPressed(_ sender: UIButton) {
         
-        playSound(soundName: sender.currentTitle!)
+        print("I got pressesd")
         
-        //Reduces the sender's (the button that got pressed) opacity to half.
-        sender.alpha = 0.5
         
-        //Code should execute after 0.2 second delay.
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            //Bring's sender's opacity back up to fully opaque.
-            sender.alpha = 1.0
         }
         
     }
     
-    func playSound(soundName: String) {
-        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
-        player = try! AVAudioPlayer(contentsOf: url!)
-        player.play()
-        
-    }
     
-}
+    
+
 
 
 
